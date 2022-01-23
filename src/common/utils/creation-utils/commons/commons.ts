91f011f99d3867bc5bea2b9967f4e35536b88ab4
fake364 <serebreanu.v@gmail.com>
2022-01-23 02:01:20 +0200

    added react-motion as dependency, added images for slider, fixed end transparency effect

--- src/common/utils/creation-utils/commons/commons.ts
+++ src/common/utils/creation-utils/commons/commons.ts
@@ -3,7 +3,7 @@ import NumericFunctions from '../../functional-utils/numeric/utils.map';
 
 const getSliderIndexArray = () => [
 	...CommonUtils.numericPalindrome(4, 1),
-	...CommonUtils.range(14).map(NumericFunctions.adjustToIndex(7))
+	...CommonUtils.range(28).map(NumericFunctions.adjustToIndex(7))
 ];
 
 const SpecificCommons = { getSliderIndexArray };
