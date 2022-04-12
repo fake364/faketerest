0efab2f56b09f00ba0391e1fe2a3b03b0a67e08d
serebreanuv <valentin.serebreanu@reedexpo.co.uk>
2022-04-12 17:13:08 +0100

    added facebook button

--- src/common/components/inputs/commonInput/CommonInput.tsx
+++ src/common/components/inputs/commonInput/CommonInput.tsx
@@ -10,19 +10,7 @@ const CommonInput: FC<CommonInputProps> = (props) => {
   return (
     <input
       {...props}
-      className={clsx(
-        props.className,
-        'rounded-form_radius',
-        'border-[#cdcdcd]',
-        'border-2',
-        'focus:outline',
-        'focus:outline-[#5DA3F6FF]',
-        'focus:outline-4',
-        'py-[8px]',
-        'px-[16px]',
-        'text-[16px]',
-        'font-light'
-      )}
+      className={clsx('w-full', props.className, 'reg-form-input')}
     />
   );
 };
--- src/components/mains/others/baseMain/regForm/form/RegForm.tsx
+++ src/components/mains/others/baseMain/regForm/form/RegForm.tsx
@@ -4,6 +4,10 @@ import { useTranslation } from 'next-i18next';
 import InputWithError from '../../../../../../common/components/inputs/commonInput/inputWithError/InputWithError';
 import PrimaryButton from '../../../../../../common/components/buttons/primary-button/PrimaryButton';
 import { useFormik } from 'formik';
+import BaseButton from '../../../../../../common/components/buttons/base/BaseButton';
+import IconButton from '../../../../../../common/components/buttons/icon-button/IconButton';
+import { IconType } from '@react-icons/all-files';
+import { FaFacebook } from '@react-icons/all-files/fa/FaFacebook';
 
 type Props = {};
 
@@ -31,7 +35,7 @@ const RegForm: React.FC<Props> = () => {
       <div className="text-[16px] text-[#333333] mt-[8px]">
         {t('regForm.form.subtitle')}
       </div>
-      <div className="flex flex-col">
+      <div className="flex flex-col w-[60%]">
         <InputWithError
           name="email"
           onChange={formik.handleChange}
@@ -51,9 +55,22 @@ const RegForm: React.FC<Props> = () => {
           className="mt-[8px]"
           placeholder="Age"
         />
-        <PrimaryButton className="mt-[8px]" onClick={formik.handleSubmit}>
+        <PrimaryButton
+          className="mt-[8px] self-stretch"
+          onClick={formik.handleSubmit}
+        >
           Continue
         </PrimaryButton>
+        <div className="mt-[8px] text-[#333333] font-bold">OR</div>
+        <BaseButton
+          className="mt-[8px] self-stretch bg-[#1877f2] rounded-common_radius py-[8px] text-white"
+          onClick={formik.handleSubmit}
+        >
+          <div className="flex items-center justify-center">
+            <FaFacebook className="mr-[8px] text-[24px]" />
+            <div>Continue with facebook</div>
+          </div>
+        </BaseButton>
       </div>
     </div>
   );
