755db4dd020b18a62e47c43b149f5a7340f9cb3b
fake364 <serebreanu.v@gmail.com>
2023-03-02 02:11:57 +0200

    changed script

--- scripts/run_project.sh
+++ scripts/run_project.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-docker-compose down -v
+docker-compose down
 
 mkdir "static-box"
 
-docker-compose up -d
+docker-compose up -d --force-recreate --build
 
 #pg_restore -c -U ${DB_USERNAME} -d ${DB_NAME} -h localhost -p ${DB_PORT} ./config/postgres-dump
